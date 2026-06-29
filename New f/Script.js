ddf268b15c7e6c93f2e641e9ee830ad4026c7e43
perthbee <perth1133@gmail.com>
2026-06-29 16:29:22 +0700

    Add files via upload

---
+++ New f/Script.js
@@ -0,0 +1,11 @@
+for(i=1;i<5;i++){
+    document.write("<h",i,">","Computer Science @ NPRU <br>","</h",i,">");
+
+}
+
+for(i=1;i<=10;i++){
+    for(j=1;j<=i;j++){
+        document.write("<img src ='cat.png'width=60 >");
+    }
+    document.write("<br>");
+}
